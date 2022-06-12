215b806286bbe624146de50d96e9a92a63544cef
ZhengKe <3119531802@qq.com>
2022-06-12 16:51:35 +0800

    feat: log4js 日志配置

--- app/config/index.ts
+++ app/config/index.ts
@@ -8,6 +8,16 @@ const config = {
     db_port: process.env.DB_PORT,
     db_password: process.env.DB_PASSWORD,
   },
+  log: {
+    appenders: {
+      cheese: { type: "file", filename: "logs/cheese.log" },
+      access: { type: "file", filename: "logs/access.log" },
+    },
+    categories: {
+      default: { appenders: ["cheese"], level: "error" },
+      access: { appenders: ["access"], level: "info" },
+    },
+  },
 };
 
 export default config;
--- app/index.ts
+++ app/index.ts
@@ -3,10 +3,11 @@ dotenv.config();
 import Koa from "koa";
 import router from "./router";
 import { Server } from "http";
+import AccessLogMiddleware from "./middleware/AccessLogMiddleware";
 
 const app = new Koa();
 
-app.use(router.routes());
+app.use(AccessLogMiddleware).use(router.routes());
 
 const run = (port: any): Server => {
   return app.listen(port);
